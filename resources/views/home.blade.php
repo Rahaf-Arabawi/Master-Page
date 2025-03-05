2746e3efbe555c1481a6fed48832746694b36f3e
Rahaf-Arabawi <rahafaqrabawi2003@gmail.com>
2025-03-05 12:47:55 +0300

    Mater-page

--- resources/views/home.blade.php
+++
@@ -1,9 +0,0 @@
-@extends('layout.layout')
-
-@section('content')
-
-<div>
-    <p>Lorem ipsum dolor sit amet, consectetur adipisicing elit. Odio maxime, possimus, nobis autem qui rerum repellendus quasi, vel delectus id neque? Alias iste, mollitia hic repellat similique corporis culpa perspiciatis.</p>
-</div>
-
-@endsection
--- routes/web.php
+++ routes/web.php
@@ -2,12 +2,14 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('home');
+Route::get('/welcome', function () {
+    return view('welcome');
 });
 
-Route::get('/about', function () {
-    return view('about');
+Route::get('/layout', function () {
+    return view('layout.layout');
 });
 
-
+Route::get('/test', function () {
+    return view('test');
+});
